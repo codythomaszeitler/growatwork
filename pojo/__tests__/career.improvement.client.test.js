4678219ef9f87d75033585e7a16c82490831632d
Cody T. Zeitler <codyzeitler12@gmail.com>
2020-03-01 20:24:24 -0700

    Get multiple hard work entries into career improvement client

--- pojo/__tests__/career.improvement.client.test.js
+++ pojo/__tests__/career.improvement.client.test.js
@@ -3,11 +3,24 @@ import {HardWorkEntry} from '../hard.work.entry';
 
 describe('Career Improvement Client', () => {
     it('should be able to track all hard work entries', () => {
+        const timestamp = new Date();
         const testObject = new CareerImprovementClient();
 
-        testObject.log(new HardWorkEntry('This is an accomplishment!', new Date()));
+        testObject.log(new HardWorkEntry('First!', timestamp));
+        testObject.log(new HardWorkEntry('Second!', timestamp));
+        testObject.log(new HardWorkEntry('Third!', timestamp));
 
         const entries = testObject.getHardWork();
-        expect(entries.length).toBe(1);
+        expect(entries.length).toBe(3);
+
+        expect(entries).toContainEqual(
+            new HardWorkEntry('First!', timestamp)
+        );
+        expect(entries).toContainEqual(
+            new HardWorkEntry('Second!', timestamp)
+        );
+        expect(entries).toContainEqual(
+            new HardWorkEntry('Third!', timestamp)
+        );
     });
 });
