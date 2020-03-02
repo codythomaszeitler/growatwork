02ddd3f42cb736f4598f33250dad3e5819e7cb21
Cody T. Zeitler <codyzeitler12@gmail.com>
2020-03-01 23:07:35 -0700

    Get dashboard screen to use dynamic list

--- screens/__tests__/dashboard.screen.test.js
+++ screens/__tests__/dashboard.screen.test.js
@@ -1,20 +1,56 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import {DashboardScreen} from '../dashboard.screen';
-import {configureEnzyme} from '../../setupTest';
-import {HardWorkEntry} from '../../pojo/hard.work.entry';
+import React from "react";
+import { create, act } from "react-test-renderer";
+import { DashboardScreen } from "../dashboard.screen";
+import { configureEnzyme } from "../../setupTest";
+import { ListItem } from "react-native-elements";
+import { HardWorkEntry } from "../../pojo/hard.work.entry";
+import { CareerImprovementClient } from "../../pojo/career.improvement.client";
 
-describe('Dashboard view information', () => {
+describe("Dashboard view information", () => {
+  let testObject;
+  beforeEach(() => {
+    configureEnzyme();
+  });
 
-    beforeEach(() => {
-        configureEnzyme();
-    });
+  it("should load all hard work entries found in the initial sleep consultant client", () => {
+    let careerImprovementClient = new CareerImprovementClient();
 
-    it('should display all entries from career improvement client', () => {
-        const component = shallow(<DashboardScreen></DashboardScreen>);
+    testObject = create(
+      <DashboardScreen
+      careerImprovementClient={careerImprovementClient}
+      ></DashboardScreen>
+    );
+  });
 
-        const hardWorkEntry = new HardWorkEntry("This is my accomplishment", new Date());
-        component.add(hardWorkEntry);
-    });
+  it("should be able to add a hard work entry", () => {
+    testObject = create(<DashboardScreen></DashboardScreen>);
+
+    const toAdd = new HardWorkEntry("This is an achievement", new Date());
+    testObject.getInstance().add(toAdd);
+
+    expect(getDisplayedHardWork()).toContainEqual(displayConversion(toAdd));
+  });
+
+  function displayConversion(hardWorkEntry) {
+    return {
+      achievement: hardWorkEntry.getAccomplishment(),
+      date: hardWorkEntry.getAccomplishedOn().toString()
+    };
+  }
+
+  function getDisplayedHardWork() {
+    const textBoxes = testObject.root.findAllByType(ListItem);
+
+    const displayed = [];
+    for (let i = 0; i < textBoxes.length; i++) {
+      const textBox = textBoxes[i];
+
+      displayed.push({
+        achievement: textBox.instance.props.title,
+        date: textBox.instance.props.subtitle
+      });
+    }
+
+    return displayed;
+  }
 });
-
--- screens/dashboard.screen.js
+++ screens/dashboard.screen.js
@@ -1,89 +1,49 @@
 import React, { Component } from "react";
-import { View, FlatList } from "react-native";
-import {ListItem} from 'react-native-elements';
-
-const list = [
-  {
-    name: "Grow and Thrive at Work"
-  },
-  {
-    name: "Amy Farha",
-    subtitle: "Vice President"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-  {
-    name: "Chris Jackson",
-    subtitle: "Vice Chairman"
-  },
-];
+import { View, FlatList, Text } from "react-native";
+import { ListItem } from "react-native-elements";
 
 export class DashboardScreen extends Component {
-
   constructor(props) {
     super(props);
     this.props = props;
+    console.log(this.props);
+    this.state = {
+      list: []
+    };
   }
 
   add(entry) {
-
+    this.setState({
+      list: this.state.list.concat({
+        name: entry.getAccomplishment(),
+        subtitle : entry.getAccomplishedOn().toString()
+      })
+    });
   }
 
   render() {
     return (
       <View>
-        <FlatList data={list} renderItem={({item}) => 
-          <View>
-
-          </View>
-        }>
-        </FlatList>
+        <FlatList
+          data={this.state.list}
+          renderItem={({ item }) => (
+            <ListItem
+              key={item}
+              title={item.name}
+              titleStyle={{
+                marginLeft: 25
+              }}
+              subtitle={item.subtitle}
+              bottomDivider
+              chevron
+              friction={90}
+              tension={100}
+              subtitleStyle={{
+                marginLeft: 65
+              }}
+            />
+          )}
+        ></FlatList>
       </View>
     );
   }
